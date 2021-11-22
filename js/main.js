e3d8604954e0df06d18eebedc26432b033f4fc7c
MrThaiTon <gunnerboomber1995@gmail.com>
2021-11-22 23:28:04 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('hello')
+import axiosClient from './api/axiosClient'
+import postApi from './api/postApi'
+
+console.log('hello from main.js')
+
+async function main() {
+  /*  const response = await axiosClient.get('/posts') */
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 5,
+    }
+    const response = await postApi.getAll(queryParams)
+
+    console.log(response)
+  } catch (error) {}
+  await postApi.updateFormData({
+    id: 'sktwi1cgkkuif36do',
+    title: 'Adipisci a enim 444',
+  })
+}
+main()
